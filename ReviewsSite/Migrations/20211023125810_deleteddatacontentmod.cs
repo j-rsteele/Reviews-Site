13442dad1c99da36f5626af7be33fb6776ac0186
Jenna Steele <jsteele021891@gmail.com>
2021-12-13 17:57:17 -0500

    initial commit

---
+++ ReviewsSite/Migrations/20211023125810_deleteddatacontentmod.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ReviewsSite.Migrations
+{
+    public partial class deleteddatacontentmod : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
